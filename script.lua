f0cbf11b38a5e64ce1a62a61f013fa067b3e79a4
KikoEmon <166333099+KikoEmon@users.noreply.github.com>
2024-05-01 11:16:20 +0700

    script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
 gg.toast("⛔ILLEGAL SCRIPT!⛔⛔")
 gg.alert("Crack Script Detected!! Please use Original Script!!")
-print("⛔Ilegal Crack Script⛔\n - User: Memo\n - Device ID: 508D92TDKM811HO8\n\n Your device will no longer be able to execute WhooTao scripts forever.\n\n - Contact: @SaikyoMaou")
+print("⛔Ilegal Crack Script⛔\n - User Script: Memo\n - Device ID: 508D92TDKM811HO8\n\n Your device will no longer be able to execute WhooTao scripts forever.\n\n - Contact: @SaikyoMaou")
 os.exit()
